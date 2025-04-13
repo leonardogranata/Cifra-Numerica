afe5a0e43b85d918a99eb0e7ed9495883bb47b96
Leonardo Jordão Granata <granataleonardo27@gmail.com>
2025-04-13 15:26:29 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+
+def encriptarMensagem(mensagem):
+    encriptado = []
+    for letra in mensagem.upper():  # Padroniza para maiúsculo
+        if letra.isalpha() and letra.isascii():  # Apenas letras A-Z
+            encriptado.append((ord(letra) - ord('A') + 1))
+        elif letra.isspace():
+            encriptado.append('/')
+        # ignora caracteres especiais e acentuados
+
+    time.sleep(1)
+
+    print(*encriptado)
+    if '/' in encriptado:
+        print("OBS: o '/' significa que a mensagem contém um espaço\nO programa ignora acentuação e caracteres especiais")
+
+if __name__ == "__main__":
+    mensagem = input("Digite a mensagem a ser encriptada: ")
+    encriptarMensagem(mensagem)
